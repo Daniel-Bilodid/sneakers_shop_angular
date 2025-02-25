f7146bbd0e55bb48c2d88dbc4900309383a3466e
Daniel <danielbeloded@gmail.com>
2025-02-25 19:03:50 +0200

    finished with navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,44 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { initializeApp } from 'firebase/app';
+import {
+  Firestore,
+  getFirestore,
+  collection,
+  getDocs,
+} from 'firebase/firestore';
+import { environment } from '../environments/environment';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss'],
+  imports: [NavbarComponent],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ecommerce-frontend';
+  private firestore!: Firestore;
+
+  constructor() {
+    const firebaseApp = initializeApp(environment.firebaseConfig);
+    this.firestore = getFirestore(firebaseApp);
+  }
+
+  async ngOnInit(): Promise<void> {
+    try {
+      const productsCollection = collection(this.firestore, 'products');
+      const snapshot = await getDocs(productsCollection);
+
+      if (snapshot.empty) {
+        console.log('Коллекция "products" пуста.');
+      } else {
+        snapshot.forEach((doc) => {
+          console.log('Документ:', doc.id, doc.data());
+        });
+      }
+    } catch (error) {
+      console.error('Ошибка при получении данных:', error);
+    }
+  }
 }
